c9afe96817219cb952e81eee73bb4133134f10fe
J-dev740 <tcr19ec029@gectcr.ac.in>
2023-08-18 01:42:46 +0530

    updated constructor test for DyanmicNft.test.js

---
+++ test/unit/DynamicNft.test.js
@@ -0,0 +1,35 @@
+const {assert,expect}=require("chai")
+const {getNamedAccounts,deployments,ethers,network,provider}=require("hardhat")
+const {networkConfig,developmentChains}=require("../../helper-hardhat-config")
+
+
+!developmentChains.includes(network.name)
+? describe.skip()
+:
+
+describe("DynamicNft Unit Test",()=>{
+
+
+let deployer,DynamicNft,PriceFeedMock
+    beforeEach(async()=>{
+        const accounts = await ethers.getSigners()
+        const account= accounts[0].address
+        deployer= await ethers.getSigner(account)
+        await deployments.fixture("all")
+        const Mock= await deployments.get("MockV3Aggregator")
+        const DN= await deployments.get("DynamicNft")
+        const DN_address= DN.address
+        const Mock_address= Mock.address
+             PriceFeedMock= await ethers.getContractAt("MockV3Aggregator",Mock_address,deployer)
+              DynamicNft= await ethers.getContractAt("DynamicNft",DN_address, deployer)
+    })
+
+    describe("constructor",()=>{
+        it("should properly convet both high and low svg to image uri",async()=>{
+            const highImageUri= await DynamicNft.getHighSvg()
+            const lowImageUri= await DynamicNft.getLowSvg()
+            assert(highImageUri.includes("data:image/svg+xml;base64,"))
+            assert(lowImageUri.includes("data:image/svg+xml;base64,"))
+        })
+    })
+})
